0d565aebab79b6adc1c626f6cc3831dfc88f083e
Nick <nickfriedrich@outlook.com>
2023-11-03 08:56:45 +0100

    basic

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,30 @@ mod templates;
 
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
+use tracing::info;
+use tracing_subscriber;
 
 use templates::HelloTemplate;
 
 #[tokio::main]
 async fn main() {
+    // For logs
+    tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/", get(hello));
+    // Setting routes
+    let app = Router::new().route("/", get(root_handler));
 
+    // Setting port
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("Listening on {}", addr);
-
+    info!("Listening on {}", addr);
+    // Starting sever
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
-
-pub async fn hello() -> HelloTemplate<'static> {
-    HelloTemplate { name: "world" }
+pub async fn root_handler() -> HelloTemplate<'static> {
+    HelloTemplate { name: "wanker" }
 }
+
--- src/templates.rs
+++ src/templates.rs
@@ -1,9 +1,7 @@
 use askama::Template;
 
-
 #[derive(Template)]
 #[template(path = "hello.html")]
 pub struct HelloTemplate<'a> {
     pub name: &'a str,
 }
-
