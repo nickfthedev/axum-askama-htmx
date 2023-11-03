f19e00abce464c1e52b479ffb8a540f3b6908a26
Nick <nickfriedrich@outlook.com>
2023-11-03 09:50:01 +0100

    added tailwind, htmx
    working example

---
+++ readme.md
@@ -0,0 +1,13 @@
+## Axum-Askama-Template
+
+### Tailwind make CSS
+
+'npm run css-dev'
+
+### Run Cargo Watch (must be installed)
+
+Install
+'cargo install cargo-watch'
+
+Run:
+'cargo watch -x run'  OR  'npm run dev'
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 mod templates;
 
-use axum::{routing::get, Router};
+use axum::{
+    handler::HandlerWithoutStateExt,
+    http::StatusCode,
+    routing::{get, post},
+    Router,
+};
 use std::net::SocketAddr;
+use templates::HelloTemplate;
+use tower_http::services::ServeDir;
 use tracing::info;
 use tracing_subscriber;
 
-use templates::HelloTemplate;
-
+//
 #[tokio::main]
 async fn main() {
     // For logs
     tracing_subscriber::fmt::init();
 
     // Setting routes
-    let app = Router::new().route("/", get(root_handler));
+    let app = app_router();
 
     // Setting port
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -25,7 +31,24 @@ async fn main() {
         .unwrap();
 }
 
+// The app router is the router for the app
+fn app_router() -> Router {
+    async fn handle_404() -> (StatusCode, &'static str) {
+        (StatusCode::NOT_FOUND, "Not found")
+    }
+
+    // you can convert handler function to service
+    let service = handle_404.into_service();
+
+    let serve_dir = ServeDir::new("public").not_found_service(service);
+
+    Router::new()
+        .route("/foo", get(|| async { "Hi from /foo" }))
+        .route("/clickme", post(|| async { "Hi HTMX" }))
+        .route("/", get(root_handler))
+        .fallback_service(serve_dir)
+}
+
 pub async fn root_handler() -> HelloTemplate<'static> {
     HelloTemplate { name: "wanker" }
 }
-
